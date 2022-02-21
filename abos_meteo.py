c240352b56e65a287a4cba4aa1cb49df397fe6df
EnRootMT <99641323+EnRootMT@users.noreply.github.com>
2022-02-21 13:54:28 +0100

    Create abos_meteo.py

---
+++ abos_meteo.py
@@ -0,0 +1,73 @@
+import re
+import time
+import argparse
+import requests
+import json
+from googleapiclient.discovery import build
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+
+
+youtube = build('youtube', 'v3', developerKey='CLE_APIYT')
+request = youtube.channels().list(part='statistics', id='ID_CHAINE' )
+response: object = request.execute()
+
+
+def demo(n, block_orientation, rotate, inreverse):
+    # create matrix device
+
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
+
+    # start demo
+     
+    i = 0
+    while i == 0 :
+        
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=VILLE&appid=CLE_API_METEO&units=metric'
+        responsemeteo = requests.get(url)
+        data = json.loads(responsemeteo.text)
+
+        temp = data['main']['temp']
+        tempdisplay = str(temp)
+        
+        response: object = request.execute()
+        abo = response['items'][0]['statistics']['subscriberCount']
+        abok = str(float(float(abo)/1000))+"K"
+
+        j = 0
+        
+        while j < 4 :
+            
+            with canvas(device) as draw:
+                text(draw, (0, 0), abok, fill="white", font=proportional(CP437_FONT))
+            time.sleep(10)
+        
+            with canvas(device) as draw:
+                text(draw, (0, 0), tempdisplay, fill="white", font=proportional(CP437_FONT))
+            time.sleep(5)
+            
+            j = j+1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='matrix_demo arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0, 1=90, 2=180, 3=270')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+
+    args = parser.parse_args()
+
+    try:
+        demo(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
+    except KeyboardInterrupt:
+        pass
